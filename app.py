34bf2a146524402deebb20a4176637ecf967178e
vijaykumarrpai <vijaykumarrpai@gmail.com>
2020-04-08 20:17:45 +0530

    heroku deployment v12

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def get_caption(filename):
     return render_template("caption.html", captioned_image = 'Flicker8k_Dataset/' + filename, caption = caption)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
